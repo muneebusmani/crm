fd6668d2bcb5f0e9bc68322daa147f3dfbbc69f9
muneebusmani <muneebusmani8355@gmail.com>
2025-09-10 00:32:01 +0500

    fixing turbopack issues in build

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,10 +1,10 @@
-import path from "node:path";
+// import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.join(__dirname, "../.."),
-  },
+  // turbopack: {
+  // root: path.join(__dirname, "../.."),
+  // },
   experimental: {
     globalNotFound: true,
   },
