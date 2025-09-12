7225e0760017d31ba70da273297a59679f8eb821
hamzaalamlinux <58773702+hamzaalamlinux@users.noreply.github.com>
2025-09-13 03:03:39 +0500

    remove  extra funciton

--- packages/types/src/types/Api.ts
+++ packages/types/src/types/Api.ts
@@ -7,13 +7,3 @@ export type ApiResponse<T> = {
 export type LoginResponse<T> = Promise<
   ApiResponse<{ data: T; accessToken: string }>
 >;
-
-
-export function successResponse<T>(data: T): ApiResponse<T> {
-  return { success: true, data };
-}
-
-export function errorResponse(error: unknown): ApiResponse<null> {
-  const message = error instanceof Error ? error.message : 'Unknown error';
-  return { success: false, error: message };
-}
