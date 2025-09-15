d6723b8923ccb875fd730bd0175628d024b02c9d
Muneeb Usmani <muneebusmani8355@gmail.com>
2025-09-15 16:43:28 +0500

    Update apps/backend/src/mailer/mailer.module.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- apps/backend/src/mailer/mailer.module.ts
+++ apps/backend/src/mailer/mailer.module.ts
@@ -6,29 +6,29 @@ import { MailerService } from "./mailer.service";
 
 @Global()
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        service: 'gmail',
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
-      },
-      defaults: {
-        from: '"CRM Quotation System" <no-reply@crm.com>',
-      },
-      template: {
-        dir: join(process.cwd(), 'src/templates'), // 👈 remove 'apps/backend' from path
-        adapter: new HandlebarsAdapter(), // use Handlebars
-        options: {
-          strict: true,
-        },
-      },
-    }),
-  ],
-  providers: [MailerService],
-  exports: [MailerService],
+	imports: [
+		MailerModule.forRoot({
+			transport: {
+				service: "gmail",
+				auth: {
+					user: process.env.MAIL_USER,
+					pass: process.env.MAIL_PASS,
+				},
+			},
+			defaults: {
+				from: '"CRM Quotation System" <no-reply@crm.com>',
+			},
+			template: {
+				dir: join(process.cwd(), "src/templates"), // 👈 remove 'apps/backend' from path
+				adapter: new HandlebarsAdapter(), // use Handlebars
+				options: {
+					strict: true,
+				},
+			},
+		}),
+	],
+	providers: [MailerService],
+	exports: [MailerService],
 })
 export class CustomMailerModule {}
 
