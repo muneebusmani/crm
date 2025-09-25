945697fdb9007226af73e6b1662cdac040edfb6d
muneeb <muneebusmani8355@gmail.com>
2025-09-25 18:06:33 +0500

    some more changes

---
+++ apps/frontend/src/app/api/logout/route.ts
@@ -0,0 +1,11 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const cookieStore = await cookies();
+  cookieStore.getAll().forEach((c) => {
+    console.log('Cookie ===>', c);
+    cookieStore.delete(c.name);
+  });
+  return NextResponse.redirect(new URL('/login', 'http://localhost:3000')); // replace with your domain in production
+}
